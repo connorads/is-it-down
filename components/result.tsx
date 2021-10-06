f17cb8b177efb15cd8f02b7df11fe8388377bb67
Connor Adams <connorads@users.noreply.github.com>
2021-10-06 17:53:59 +0100

    Add loading/retry animation and retry count

--- components/result.tsx
+++ components/result.tsx
@@ -1,6 +1,8 @@
 import useSWR from 'swr'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import styles from '../styles/Result.module.css'
+
 
 const fetcher = async (
     input: RequestInfo,
@@ -15,20 +17,27 @@ const fetcher = async (
 const Result = ({ url }: { url: string }) => {
     const params = new URLSearchParams()
     params.set("url", url)
-    const { data, error } = useSWR<string, Error>(`/api/isit?${params}`, fetcher)
+    const { data, error, isValidating } = useSWR<string, Error>(`/api/isit?${params}`, fetcher)
+
+    const [retryCount, setRetryCount] = useState(0);
 
     useEffect(() => {
         console.debug("error", error)
         console.debug("data", data)
     }, [error, data])
 
+    useEffect(()=> {
+        console.debug("isValidating", isValidating)
+        if (isValidating) setRetryCount(r => r + 1);
+    }, [isValidating])
+
     if (error) return <>
-        <p>Maybe it is down 👀</p>
-        <p><code >{error.message}</code></p>
+        <p className={styles.loading}>Maybe it is down 👀 Retry {retryCount}</p>
+        <p><code>{error.message}</code></p>
         <Image width={498} height={249} alt="GIF of poor TV signal" src="/error.gif"></Image>
     </>
     if (!data) return <>
-        <p>Loading, please wait ...</p>
+        <p className={styles.loading}>Loading, please wait</p>
         <Image width={498} height={249} alt="GIF of dial-up modem" src="/loading.gif"></Image>
     </>
     return <>
